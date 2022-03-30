1ed797f76c825dd0942e55115068582a1809fe4b
nickdaniels <n1daniels@student.bridgew.edu>
2022-03-30 15:34:01 -0400

    learning

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-def open_file(filename):
-    file = open(filename, "r")
-    lines = file.readline()
+
+def open_file(file_name):
+    file = open(file_name,'r', encoding='utf-8')
+    lines = file.readlines()
     return lines
 
-def main():
-   file_lines = open_file("warpiece.txt")
-   word_count(file_lines)
 
-
-def display_results(word_counts):
+def display_result(word_counts):
     for word in word_counts:
-        count = word_counts(word)
-        print(f"{word}: t/ {count}")
+        count = word_counts[word]
+        print(f"{word}:\t {count}")
 
 
+def main():
+    file_lines = open_file("warpiece.txt")
+    word_counts = word_count(file_lines)
+    display_result(word_counts)
 
 def word_count(all_lines):
     word_counter = {}
     for line in all_lines:
-        words_in_line = line.split(" ")
+        words_in_line = line.split(' ')
         for word in words_in_line:
             if word in word_counter:
-                word_counter[word] += 1
-            else:
-                word_counter[word]=1
 
+                word_counter[word]+= 1
+            else:
+                word_counter[word] = 1
     return word_counter
 
-
+main()
 
 
 main()
